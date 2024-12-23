621ed2f1a32b5e8cd6556e3f404af49eff2060d9
Vincent Huang <VincentHuang124@gmail.com>
2024-12-22 20:42:58 -0500

    backend updates

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        fullName: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+        profilePic: {
+            type: String,
+            default: "",
+        },
+    },
+
+    {timestamps: true} // To show when they were a member
+);
+
+const User = mongoose.model("User", userSchema); // Creating user model with mongoose with required schema
+
+export default User;
