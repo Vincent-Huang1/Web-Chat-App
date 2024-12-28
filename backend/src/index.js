b820a866cb92155caa71e5e948f097feb5eb1491
Vincent Huang <VincentHuang124@gmail.com>
2024-12-27 21:09:16 -0500

    updated profile page and increased upload file size to 50mb

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,8 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' })); // Increase the file limit size to 50mb
+
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
