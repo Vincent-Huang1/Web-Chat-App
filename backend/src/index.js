b26a039621f3dbedf476e531c8518762e1a20eef
Vincent Huang <VincentHuang124@gmail.com>
2024-12-29 22:30:56 -0500

    project complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,12 @@ import messageRoutes from "./routes/message.route.js";
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import { app, server } from './lib/socket.js';
 
 dotenv.config();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '50mb' })); // Increase the file limit size to 50mb
 
@@ -19,6 +21,15 @@ app.use(cors({
 }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"));
+    })
+}
+
 server.listen(PORT, () => {
     console.log("server is running on port: " + PORT);
     connectDB();
